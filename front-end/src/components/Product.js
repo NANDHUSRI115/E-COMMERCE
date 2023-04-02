6c84b45f84db170fd52e2a2e80880fdae60f458e
NANDHUSRI115 <nandhusri115@gmail.com>
2023-04-02 14:27:51 +0530

    Product and Rating

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -1,20 +1,25 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
+
 function Product(props) {
   const { product } = props;
   return (
-    <div className="product" key={product.slug}>
+    <Card>
       <Link to={`/product/${product.slug}`}>
-        <img src={product.image} alt={product.name} />
+        <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
-      <div className="product-info">
+      <Card.Body>
         <Link to={`/product/${product.slug}`}>
-          <p> {product.name} </p>
+          <Card.Title>{product.name}</Card.Title>
         </Link>
-        <p>
-          <strong> ${product.price} </strong>
-        </p>
-        <button>Add to cart</button>
-      </div>
-    </div>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
   );
 }
+
 export default Product;
