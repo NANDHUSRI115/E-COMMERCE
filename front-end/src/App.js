89c419a3f0ce5fa485db11449ff614b7e4b7f409
NANDHUSRI115 <nandhusri115@gmail.com>
2023-04-02 16:19:00 +0530

    Card items

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,14 +19,14 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>amazon clone</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                      {cart.cartItems.length}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
