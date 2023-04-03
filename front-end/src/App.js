3f7953593d79c1e209029814e24a0f4bdf7731d9
NANDHUSRI115 <nandhusri115@gmail.com>
2023-04-03 16:22:54 +0530

    signin and jwt

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>Amazon clone</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
